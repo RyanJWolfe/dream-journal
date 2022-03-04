4d32627c400e20b64a3c0e7b7e2b2f6aef2f82d8
Ryan Wolfe <xryanwolfe@gmail.com>
2022-03-04 16:02:08 -0700

    Generate devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users
   resources :journals do
     resources :dream_logs
   end
