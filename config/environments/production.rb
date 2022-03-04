f172417fe3642fb716a4ca866c1caa1ac26fccd2
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-03-03 18:24:00 -0700

    Install devise
    - Add notice and alert to application level
    - Add tailwind components for notice and alert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,9 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Set default url for devise mailer
+  config.action_mailer.default_url_options = { host: 'https://pacific-dawn-84126.herokuapp.com' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
