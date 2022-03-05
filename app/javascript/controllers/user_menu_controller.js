13d5f92047f55711666d7f200dedd300f203f49b
Ryan Wolfe <xryanwolfe@gmail.com>
2022-03-05 16:26:44 -0700

    User menu dropdown styling

--- app/javascript/controllers/user_menu_controller.js
+++ app/javascript/controllers/user_menu_controller.js
@@ -3,7 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["dropdown"]
 
-  toggle() {
+  toggle(e) {
+    e.stopPropagation()
     this.dropdownTarget.classList.toggle('hidden')
+  }
+
+  hide() {
+    this.dropdownTarget.classList.add('hidden')
   }
 }
