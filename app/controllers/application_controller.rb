1f055551c4083f3f9e49ceae4b54cb32ecc06cbb
Ryan Wolfe <xryanwolfe@gmail.com>
2022-03-10 18:33:55 -0700

    Create stub for setting timezone based on user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
+# Application controller, sets time zone based on signed in user's timezone
 class ApplicationController < ActionController::Base
+  before_action :set_time_zone, if: :user_signed_in?
+
+  private
+
+  def set_time_zone
+    # TODO: setup user model to store time zones
+    # Time.zone = current_user.time_zone
+  end
 end
